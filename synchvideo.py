ad1d8291d20d31e39fa9460ca60b1e0d42fa6c9b
Craig Disselkoen <craigdissel@gmail.com>
2020-03-05 10:52:53 -0800

    first commit

---
+++ synchvideo.py
@@ -0,0 +1,26 @@
+import arena
+import json
+
+def callback(msg):
+    jsonMsg = json.loads(msg)
+    if jsonMsg["action"] != "clientEvent":
+        return
+    if jsonMsg["object_id"] == "videoscreen":
+        print(jsonMsg["type"])
+
+arena.init(
+    "arena-west1.conix.io",
+    "realm",
+    "synchvideo",
+    port = 3003,
+    callback = callback,
+)
+arena.Object(
+    objName = "videoscreen",
+    objType = arena.Shape.cube,
+    scale = (16, 9, 0.1),
+    location = (0, 5, -15),
+    data = '{"material": {"src": "images/360falls.mp4"}}',
+    clickable = True,
+)
+arena.handle_events()
